b8d9654bf10905e654a4d43506c33839b05fe61d
santiagor529 <94879223+santiagor529@users.noreply.github.com>
2023-04-24 16:15:49 +0000

    Modified the code
    I wanted the code to keep running even if an exception was raised so I created a while loop that keeps going until the user quits, or until they get the right file path.

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import os
 
 Filenames = []
 Filetypes = {}
-
-Filepath = input('Input the file path: ')
-Directory = Filepath
-try:
-  ListDirectory = os.listdir(Directory)
-except:
-  print('Not a valid file path. The folder was not found.')
-  
-print(ListDirectory)
+# Creating a while loop so that the program doesn't just end if wrong input
+Tries = 0
+while Tries == 0:
+# Input a filepath and then the files in that folder get listed out
+  Filepath = input('Input the file path: ').lower()
+  Directory = Filepath
+# Tries to list the directory inputed and if there's an error it lets the user retry
+  try:
+    ListDirectory = os.listdir(Directory)
+    print(ListDirectory)
+    Tries += 1
+  except:
+    print('Not a valid file path. The folder was not found.')
+    Retry = input('Would you like to try again? Y/N ')
+    if Retry.lower() != 'y':
+      Tries += 1
